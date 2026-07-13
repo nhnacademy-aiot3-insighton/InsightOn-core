eaf65b19c0deb67d6c936dbd88ade37174750ee0
OiJs <oijs9663@gmail.com>
2026-07-13 10:08:16 +0900

    feat: firstCommit

---
+++ src/main/java/com/insighton/core/InsightOnCoreApplication.java
@@ -0,0 +1,13 @@
+package com.insighton.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsightOnCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsightOnCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/insighton/core/InsightOnCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insighton.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightOnCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
