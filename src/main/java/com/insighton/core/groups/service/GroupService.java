cf99a526905e699e0cce9edc2b55c0e09c2a6d8b
naeun912 <st243235@gmail.com>
2026-07-16 18:41:51 +0900

    feat: group service 추가 구현

--- src/main/java/com/insighton/core/groups/service/GroupService.java
+++ src/main/java/com/insighton/core/groups/service/GroupService.java
@@ -2,38 +2,50 @@ package com.insighton.core.groups.service;
 
 
 import com.insighton.core.groups.dto.request.GroupCreateRequest;
+import com.insighton.core.groups.dto.response.GroupListResponse;
 import com.insighton.core.groups.dto.response.GroupResponse;
+
+import java.util.List;
 
 public interface GroupService {
     /**
      * 그룹 생성
      * @param request Group 생성 요청 정보
      */
-    void createGroup(GroupCreateRequest request);
+    void createGroup(GroupCreateRequest request, Long userId);
 
     /**
      * 관리자용 group 정보 조회
-     * @param inviteToken 그룹 초대 토큰
+     * @param userId login한 user의 ID
      * @return 그룹 정보
      */
-    GroupResponse getGroupByInviteToken(String inviteToken);
+    GroupResponse getGroupAdmin(Long userId, Long groupId);
 
     /**
      * 일반 사용자의 초대한 그룹 조회용 (초대장 느낌)
      * 이 초대 토큰은 A 회사의 초대 토큰 입니다 하고 회사 정보를 띄우기.(토큰은 null 값으로 들어감)
      * @param inviteToken 초대 코드
+     * @param userId 로그인한 user의 ID
+     * @param groupId 내가 지금 보고 있는 group의 ID
      * @return 토큰 값 빼고 group 정보가 들어감
      */
-    GroupResponse getGroupByToken(String inviteToken);
+    GroupResponse getGroupPreview(String inviteToken, Long userId, Long groupId);
 
     /**
      * 현재 로그인한 사용자의 소속 그룹 정보 조회
      * (한 계정당 하나의 그룹만 가입 가능하므로, 로그인 정보 기반으로 해당 그룹 정보를 반환)
-     * @param groupId 그룹 id
+     * @param userId login한 user의 ID
      * @return token 정보를 제외한 group의 정보
      */
-    GroupResponse getMyGroup(Long groupId);
+    GroupResponse getMyGroup(Long userId, Long groupId);
 
+    /**
+     * 시스템 관리자가 group List를 조회
+     * @param userRole 로그인한 사용자의 권한...?
+     * @param userId 로그인한 user ID
+     * @return GroupList 반환
+     */
+    List<GroupListResponse> getGroupList(String userRole, Long userId);
 
     /**
      * 토큰 재발급
