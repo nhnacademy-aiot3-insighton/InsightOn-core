d22b1ced980487668b0e65ce0fa3b8abb29bbec7
naeun912 <st243235@gmail.com>
2026-07-20 16:56:20 +0900

    feat: method에 대한 주석과 groupMember생성, 그룹 멤버 권한 확인용 method, 멤버 권한 변경 method 구현

--- src/main/java/com/insighton/core/groupmember/service/GroupMembersService.java
+++ src/main/java/com/insighton/core/groupmember/service/GroupMembersService.java
@@ -3,6 +3,7 @@ package com.insighton.core.groupmember.service;
 
 import com.insighton.core.groupmember.dto.request.GroupMembersJoinRequest;
 import com.insighton.core.groupmember.entity.GroupMembers;
+import com.insighton.core.groups.entity.Groups;
 
 public interface GroupMembersService {
 
@@ -12,8 +13,34 @@ public interface GroupMembersService {
      */
     void joinGroupByToken(GroupMembersJoinRequest request);
 
+    /**
+     * 그룹 생성자. 바로 SUPER_MANAGER 권한 가지는 GroupMember 생성
+     * @param groups 생성된 group의 정보
+     * @param userId login한 user의 id
+     */
+    void createGroupMember(Groups groups, Long userId);
+
+    /**
+     * 특정 그룹에 속한 멤버인지 검증하고 멤버 객체 반환
+     */
     GroupMembers validateGroupMembers(Long groupId, Long userId);
 
+    /**
+     * member 권한 변경
+     * @param groupId 그들이 속한 group ID
+     * @param targetUserId 권한을 변경 타겟의 user의 ID
+     * @param adminId 변경 권한을 가진 user의 ID
+     */
+    void toggleManagerRole(Long groupId, Long targetUserId, Long adminId);
+
+    /**
+     * 시스템에 존재하는 올바른 유저인지 확인
+     */
     void validateUserExists(Long userId);
+
+    /**
+     * 그룹 멤버의 권환 확인용
+     */
+    boolean isGroupAdmin(Long groupId, Long userId);
 
 }
--- src/main/java/com/insighton/core/groupmember/service/impl/GroupMembersServiceImpl.java
+++ src/main/java/com/insighton/core/groupmember/service/impl/GroupMembersServiceImpl.java
@@ -7,6 +7,9 @@ import com.insighton.core.groupmember.exception.GroupMemberNotFoundException;
 import com.insighton.core.groupmember.exception.UserIdNotFoundException;
 import com.insighton.core.groupmember.repository.GroupMembersRepository;
 import com.insighton.core.groupmember.service.GroupMembersService;
+import com.insighton.core.groups.entity.Groups;
+import com.insighton.core.groups.exception.GroupNotFoundException;
+import com.insighton.core.groups.exception.NoPermissionException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,10 +20,7 @@ public class GroupMembersServiceImpl implements GroupMembersService{
 
     private final GroupMembersRepository groupMembersRepository;
 
-    /**
-     * 그룹 가입
-     * @param request 그룹 가입 요청 DTO
-     */
+    @Override
     @Transactional
     public void joinGroupByToken(GroupMembersJoinRequest request) {
 
@@ -31,24 +31,73 @@ public class GroupMembersServiceImpl implements GroupMembersService{
         groupMembersRepository.findByGroups_GroupIdAndUserId(groupMembers.getGroups().getGroupId(), request.userId())
                 .orElseThrow(()->new AlreadyJoinedException(request.userId()));
 
-
         GroupMembers newMember = GroupMembers.builder()
                 .groups(groupMembers.getGroups())
                 .userId(request.userId())
                 .groupRole(GroupMembers.GroupRole.MEMBER)
                 .build();
 
+
         groupMembersRepository.save(newMember);
     }
 
-//     ==================== 공통 검증 및 조회용 헬퍼 메서드 ====================
+    @Override
+    @Transactional
+    public void createGroupMember(Groups groups, Long userId){
+        validateUserExists(userId);
 
+
+        GroupMembers members = GroupMembers.builder()
+                .groups(groups)
+                .userId(userId)
+                .groupRole(GroupMembers.GroupRole.SUPER_MANAGER)
+                .build();
+
+        groupMembersRepository.save(members);
+    }
+
+    @Override
+    @Transactional
+    public void toggleManagerRole(Long groupId, Long targetUserId, Long adminId) {
+        // 1. group id가 존재하는지 확인
+        GroupMembers adminMember = validateGroupMembers(groupId, adminId);
+        GroupMembers targetMember = validateGroupMembers(groupId, targetUserId);
+
+        // 2. adminID가 manager나 owner권한을 가진 자인지 확인
+        // target의 권한이 member일 경우 member는 권한이 없음
+        if(targetMember.isMember()){
+            if(adminMember.isMember()){
+                throw NoPermissionException.forAdmin(adminId);
+            }
+            // manager의 권한은 owner말고는 변경하지 못함
+        } else if (targetMember.isManager()) {
+            if(adminMember.isMember() || adminMember.isManager()){
+                throw NoPermissionException.forAdmin(adminId);
+            }
+        }else {
+            if(adminMember.isSuperManager()){
+                throw NoPermissionException.forAdmin(adminId);
+            }
+        }
+        // 3. targetUser가 Member권한일 때 Manager로 변경
+        targetMember.updateRole(targetMember.getGroupRole() == GroupMembers.GroupRole.MEMBER ? GroupMembers.GroupRole.MANAGER : GroupMembers.GroupRole.MEMBER);
+    }
+
+    //     ==================== 공통 검증 및 조회용 헬퍼 메서드 ====================
+
+    /**
+     * 그룹 멤버의 권환 확인용
+     */
+    public boolean isGroupAdmin(Long groupId, Long userId){
+        GroupMembers members = validateGroupMembers(groupId, userId);
+
+        return members.getGroupRole() == GroupMembers.GroupRole.MANAGER || members.getGroupRole() == GroupMembers.GroupRole.SUPER_MANAGER;
+    }
     /**
      * 특정 그룹에 속한 멤버인지 검증하고 멤버 객체 반환
      */
     @Override
     public GroupMembers validateGroupMembers(Long groupId, Long userId) {
-        // findByGroups_GroupIdAndUserId의 파라미터 바인딩 순서(groupId, userId) 오류를 바로잡았습니다.
         return groupMembersRepository.findByGroups_GroupIdAndUserId(groupId, userId)
                 .orElseThrow(() -> new UserIdNotFoundException(userId));
     }
