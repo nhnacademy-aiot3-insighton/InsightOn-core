734e8f5a6f7d038864713f4e1bad9c0c96df51db
naeun912 <st243235@gmail.com>
2026-07-20 10:26:38 +0900

    feat: exception 두가지 경우로 분리

--- src/main/java/com/insighton/core/exception/groupmember/GroupMemberNotFoundException.java
+++ src/main/java/com/insighton/core/exception/groupmember/GroupMemberNotFoundException.java
@@ -2,6 +2,10 @@ package com.insighton.core.exception.groupmember;
 
 public class GroupMemberNotFoundException extends RuntimeException {
     public GroupMemberNotFoundException(Long userId, Long groupId) {
-        super("Not found ");
+        super("Not found Group Member. User ID: " + userId + "Group ID: " + groupId);
+    }
+
+    public GroupMemberNotFoundException(Long userId) {
+        super("Not found Group Member. User ID: " + userId);
     }
 }
