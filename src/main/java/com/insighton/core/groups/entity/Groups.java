fa53d78ea8a8371a22458634c124879aa78bb828
naeun912 <st243235@gmail.com>
2026-07-22 17:54:15 +0900

    feat: 주석처리 풀기

--- src/main/java/com/insighton/core/groups/entity/Groups.java
+++ src/main/java/com/insighton/core/groups/entity/Groups.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
 @Entity
-//@Table(name = "groups")
+@Table(name = "groups")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Groups {
