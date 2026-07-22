4f2762da7f2cd7170faf1a70cf64c6878b280842
dayea11 <dayea021128@gmail.com>
2026-07-22 09:50:30 +0900

    docs: location service 에 사용자 위치 설정 service 추가

--- src/main/java/com/insighton/core/location/service/LocationService.java
+++ src/main/java/com/insighton/core/location/service/LocationService.java
@@ -1,40 +1,58 @@
 package com.insighton.core.location.service;
 
 import com.insighton.core.exception.LocationNotFoundException;
+import com.insighton.core.location.dto.GroupLocationDto;
 import com.insighton.core.location.dto.LocationGridDto;
-import com.insighton.core.location.repository.InMemoryLocationRegistry;
-import com.insighton.core.location.repository.LocationRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
+import com.insighton.core.location.dto.LocationRequestDto;
+import com.insighton.core.location.dto.LocationResponseDto;
+import com.insighton.core.location.registry.LocationRegistry;
+import com.insighton.core.location.repository.GroupLocationRepository;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LocationService {
 
-    private final InMemoryLocationRegistry inMemoryLocationRegistry;
-    private final LocationRepository locationRepository;
+    private final LocationRegistry locationRegistry;
+    private final GroupLocationRepository groupLocationRepository;
 
     public List<String> getSortedStates() {
-        List<String> states = locationRepository.findAllStates();
+        List<String> states = locationRegistry.findAllStates();
         Collections.sort(states); // 오름차순 정렬
         return states;
     }
 
     public List<String> getSortedCities(String state) {
-        List<String> cities = locationRepository.findCitiesByState(state);
+        List<String> cities = locationRegistry.findCitiesByState(state);
         Collections.sort(cities);
         return cities;
     }
 
-    public LocationGridDto getGridCoordinate(String state, String city) {
-        Optional<LocationGridDto> optionalLocationGridDto = locationRepository.findGridCoordinate(state, city);
+    public LocationResponseDto selectGroupLocation(LocationRequestDto requestDto) {
+        Optional<LocationGridDto> optionalLocationGridDto = locationRegistry.findGridCoordinate(
+                requestDto.step1(), requestDto.step2());
         if (optionalLocationGridDto.isEmpty()) {
             throw new LocationNotFoundException("존재하지 않는 행정구역 좌표 정보입니다.");
         }
-        return optionalLocationGridDto.get();
+
+        LocationGridDto gridDto = optionalLocationGridDto.get();
+
+        GroupLocationDto groupLocationDto = new GroupLocationDto(
+                requestDto.groupId(),
+                gridDto,
+                LocalDateTime.now()
+        );
+
+        groupLocationRepository.save(groupLocationDto);
+
+        return new LocationResponseDto(
+                groupLocationDto.groupId(),
+                groupLocationDto.locationGridDto()
+        );
     }
 }
