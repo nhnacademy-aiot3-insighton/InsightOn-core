232c6eede83182359e482352041a2f9586cc83ee
{naeun912} <{st243235@gmail.com}>
2026-07-22 10:49:00 +0900

    feat: @EnableFeignClients 어노테이션 추가

--- src/main/java/com/insighton/core/InsightOnCoreApplication.java
+++ src/main/java/com/insighton/core/InsightOnCoreApplication.java
@@ -2,8 +2,10 @@ package com.insighton.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class InsightOnCoreApplication {
 
     public static void main(String[] args) {
