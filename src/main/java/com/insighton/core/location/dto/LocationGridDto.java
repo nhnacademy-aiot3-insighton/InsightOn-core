30c4c0fb054c39990e034ea420e56479da7bc6ac
dayea11 <dayea021128@gmail.com>
2026-07-23 17:35:03 +0900

    docs: 격자 좌표 dto 이름 변경 -> 원래대로

--- src/main/java/com/insighton/core/location/dto/LocationGridDto.java
+++ src/main/java/com/insighton/core/location/dto/LocationGridDto.java
@@ -3,7 +3,7 @@ package com.insighton.core.location.dto;
 public record LocationGridDto(
         String step1,
         String step2,
-        int nx,
-        int ny
+        int gridX,
+        int gridY
 ) {
 }
