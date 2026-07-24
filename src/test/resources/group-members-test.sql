7d90d4290610b8f82e3efa1c26a0d5475478acde
{naeun912} <{st243235@gmail.com}>
2026-07-24 16:21:50 +0900

    feat: name -> group_name으로 변경

--- src/test/resources/group-members-test.sql
+++ src/test/resources/group-members-test.sql
@@ -3,8 +3,8 @@ TRUNCATE TABLE groups RESTART IDENTITY;
 TRUNCATE TABLE group_members RESTART IDENTITY;
 SET REFERENTIAL_INTEGRITY TRUE;
 
-INSERT INTO groups (name, description, location, invite_token, created_at)
+INSERT INTO groups (group_name, description, location, invite_token, created_at)
 VALUES ('test-group', 'test-description', 'test-location', 'test-token', CURRENT_TIMESTAMP);
 
 INSERT INTO group_members (group_id, user_id, group_role, joined_at)
-values ( 1, 1, 'MEMBER', CURRENT_TIMESTAMP)
+values (1, 1, 'MEMBER', CURRENT_TIMESTAMP)
