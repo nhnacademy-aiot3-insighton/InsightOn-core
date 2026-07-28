2e3f43990f6c7a81724f4ec1214db6806a30978f
naeun912 <st243235@gmail.com>
2026-07-28 16:38:28 +0900

    feat: location -> groupRegion

--- src/main/java/com/insighton/core/groups/dto/request/GroupsRequest.java
+++ src/main/java/com/insighton/core/groups/dto/request/GroupsRequest.java
@@ -9,12 +9,12 @@ import lombok.Builder;
  *
  * @param name        회사 가입 기관 또는 계열사 공식 명칭
  * @param description 해당 회사 및 사업장에 대한 세부 상세 설명
- * @param location    회사 위치
+ * @param groupRegion 회사 위치
  */
 @Builder
 public record GroupsRequest(
         @NotBlank String name,
         String description,
-        @NotBlank String location
+        @NotBlank String groupRegion
 ) {
 }
--- src/main/java/com/insighton/core/groups/dto/response/GroupsAdminResponse.java
+++ src/main/java/com/insighton/core/groups/dto/response/GroupsAdminResponse.java
@@ -8,13 +8,13 @@ import lombok.Builder;
  * @param groupId     그룹 id
  * @param name        회사 가입 기관 또는 계열사 공식 명칭
  * @param description 해당 회사 및 사업장에 대한 세부 상세 설명 (nullable 가능)
- * @param location    회사 위치 (지번)
+ * @param groupRegion 회사 위치 (지번)
  */
 @Builder
 public record GroupsAdminResponse(
         Long groupId,
         String name,
         String description,
-        String location
+        String groupRegion
 ) {
 }
