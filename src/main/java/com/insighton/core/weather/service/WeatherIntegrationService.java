8b55b78e557871cff8f3e9b03381e4fdef03b0d3
dayea11 <dayea021128@gmail.com>
2026-07-29 17:34:11 +0900

    docs: redis @Cacheable 어노테이션 추가

--- src/main/java/com/insighton/core/weather/service/WeatherIntegrationService.java
+++ src/main/java/com/insighton/core/weather/service/WeatherIntegrationService.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
@@ -47,6 +48,7 @@ public class WeatherIntegrationService {
         this.sidoNameParser = sidoNameParser;
     }
 
+    @Cacheable(value = "weatherData", key = "#sidoName", unless = "#result == null ")
     public WeatherDataDto fetchWeatherData(int gridX, int gridY, String sidoName, String cityName, String baseDate,
                                            String baseTime) {
         String parsedSidoName = sidoNameParser.parse(sidoName);
