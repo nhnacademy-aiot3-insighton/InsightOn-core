a069ee1a8fbe7ccf9f4e83214e7d1c0296afc2e7
JJ <wognlrla1@gmail.com>
2026-07-29 13:26:21 +0900

    test 코드 작성중

---
+++ src/test/java/com/insighton/core/actuator/controller/ActuatorControllerTest.java
@@ -0,0 +1,4 @@
+package com.insighton.core.actuator.controller;
+
+public class ActuatorControllerTest {
+}
---
+++ src/test/java/com/insighton/core/actuator/repositroy/ActuatorRepositroyTest.java
@@ -0,0 +1,4 @@
+package com.insighton.core.actuator.repositroy;
+
+public class ActuatorRepositroyTest {
+}
