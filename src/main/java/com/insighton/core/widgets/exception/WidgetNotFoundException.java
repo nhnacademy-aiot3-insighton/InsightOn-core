405aeb710d6205e5933a28e26cb6b8036984ac6a
{naeun912} <{st243235@gmail.com}>
2026-08-04 14:40:10 +0900

    feat: widget not found exception 작성

---
+++ src/main/java/com/insighton/core/widgets/exception/WidgetNotFoundException.java
@@ -0,0 +1,19 @@
+package com.insighton.core.widgets.exception;
+
+public class WidgetNotFoundException extends RuntimeException {
+    public WidgetNotFoundException(String message) {
+        super(message);
+    }
+
+    public static WidgetNotFoundException notFoundWidgetByDashboardId(Long dashboardId) {
+        return new WidgetNotFoundException(
+                String.format("Widget not found. Dashboard ID : " + dashboardId)
+        );
+    }
+
+    public static WidgetNotFoundException notFoundWidgetByWidgetId(Long widgetId) {
+        return new WidgetNotFoundException(
+                String.format("Widget not found. Widget ID : " + widgetId)
+        );
+    }
+}
