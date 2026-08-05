b29f85a10050ab78179e51fc5bcb15dec0c9667d
dayea11 <dayea021128@gmail.com>
2026-08-05 16:10:22 +0900

    feat: 숫자로 들어오는 기상청/에어코리아 상태 코드를 한글 문자열로 변환하는 로직

---
+++ src/main/java/com/insighton/core/weather/util/WeatherCodeMapper.java
@@ -0,0 +1,46 @@
+package com.insighton.core.weather.util;
+
+public class WeatherCodeMapper {
+
+    // 하늘상태 변환
+    public static String parseSky(String code) {
+        if (code == null) {
+            return "알수없음";
+        }
+        return switch (code) {
+            case "1" -> "맑음";
+            case "3" -> "구름많음";
+            case "4" -> "흐림";
+            default -> "알수없음(" + code + ")";
+        };
+    }
+
+    // 강수형태 변환
+    public static String parsePty(String code) {
+        if (code == null) {
+            return "없음";
+        }
+        return switch (code) {
+            case "0" -> "없음";
+            case "1" -> "비";
+            case "2" -> "비/눈";
+            case "3" -> "눈";
+            case "4" -> "소나기";
+            default -> "없음";
+        };
+    }
+
+    // 미세먼지 등급 변환
+    public static String parseAirGrade(String grade) {
+        if (grade == null) {
+            return "정보없음";
+        }
+        return switch (grade) {
+            case "1" -> "좋음";
+            case "2" -> "보통";
+            case "3" -> "나쁨";
+            case "4" -> "매우나쁨";
+            default -> "정보없음";
+        };
+    }
+}
