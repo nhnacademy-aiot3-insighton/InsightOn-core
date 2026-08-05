18a0cbdd6c94cb904c0a27c35e99db7a011062c7
naeun912 <st243235@gmail.com>
2026-08-05 15:46:58 +0900

    feat: saveDashboard api 추가

--- src/main/java/com/insighton/core/dashboards/controller/DashboardController.java
+++ src/main/java/com/insighton/core/dashboards/controller/DashboardController.java
@@ -18,22 +18,6 @@ public class DashboardController {
     private final CoreManagementUseCase useCase;
 
     /**
-     * GET /api/v1/groups/{group-id}/location/{location-id}/dashboard
-     * {
-     *   "dashboardId": 1,
-     *   "locationId": 42,
-     *   "title": "4층 개발팀",
-     *   "updatedAt": "2026-07-30T10:00:00+09:00",
-     *   "widgets": [
-     *     { "widgetId": 1, "type": "LINE_CHART", "metricKey": "co2",
-     *       "xPos": 0, "yPos": 0, "width": 6, "height": 4 },
-     *     { "widgetId": 2, "type": "GAUGE", "metricKey": "humidity",
-     *       "xPos": 6, "yPos": 0, "width": 3, "height": 4 }
-     *   ]
-     * }
-     */
-
-    /**
      * dashboard 조회
      *
      * @param userId     조회하려는 user의 ID
