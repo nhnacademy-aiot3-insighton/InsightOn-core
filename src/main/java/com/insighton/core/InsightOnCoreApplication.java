cd8a8bb995c29fcc57a0ba1bddfc0f2b4ed2d199
naeun912 <st243235@gmail.com>
2026-08-07 15:22:48 +0900

    feat: RabbitMQ 재시도를 위해 @EnableRetry 어노테이션 추가

--- src/main/java/com/insighton/core/InsightOnCoreApplication.java
+++ src/main/java/com/insighton/core/InsightOnCoreApplication.java
@@ -3,6 +3,7 @@ package com.insighton.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableScheduling
 @EnableFeignClients
+@EnableRetry
 public class InsightOnCoreApplication {
 
     public static void main(String[] args) {
