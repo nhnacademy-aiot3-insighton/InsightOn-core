1f5530477683501cee308b2fd69a923c8d58b427
dayea11 <dayea021128@gmail.com>
2026-08-13 17:58:43 +0900

    feat: 데드락,서버 다운 후 재시작시 날씨 조회하는 컨트롤러

---
+++ src/main/java/com/insighton/core/controller/api/WeatherRecoveryController.java
@@ -0,0 +1,32 @@
+package com.insighton.core.controller.api;
+
+import com.insighton.core.domain.weather.dto.WeatherDataDto;
+import com.insighton.core.domain.weather.service.WeatherService;
+import com.insighton.core.usecase.WeatherRecoveryUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/weather")
+public class WeatherRecoveryController {
+
+    private final WeatherService weatherService;
+    private final WeatherRecoveryUseCase weatherRecoveryUseCase;
+
+    @GetMapping("/group/{groupId}")
+    public ResponseEntity<WeatherDataDto> getWeatherByGroupId(
+            @PathVariable Long groupId,
+            @RequestParam String baseDate,
+            @RequestParam String baseTime
+    ) {
+        WeatherDataDto weatherDataDto = weatherRecoveryUseCase.recoveryWeather(groupId, baseDate, baseTime);
+
+        return ResponseEntity.ok(weatherDataDto);
+    }
+}
