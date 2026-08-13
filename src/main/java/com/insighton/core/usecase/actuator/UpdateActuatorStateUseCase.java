b32d7c8e3bfbafe3a9d988a194b551396b03f753
JJ <wognlrla1@gmail.com>
2026-08-13 13:49:07 +0900

    검증 로직 위치 변경 및 수정

--- src/main/java/com/insighton/core/usecase/actuator/UpdateActuatorStateUseCase.java
+++ src/main/java/com/insighton/core/usecase/actuator/UpdateActuatorStateUseCase.java
@@ -2,6 +2,8 @@ package com.insighton.core.usecase.actuator;
 
 import com.insighton.core.common.annotation.UseCase;
 import com.insighton.core.domain.actuatorrunlogs.entity.ExecutedByType;
+import com.insighton.core.domain.actuators.dto.ActuatorResponse;
+import com.insighton.core.domain.actuators.policy.ActuatorCommandPreset;
 import com.insighton.core.domain.actuators.service.ActuatorService;
 import com.insighton.core.domain.groupmember.service.GroupMemberService;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +21,11 @@ public class UpdateActuatorStateUseCase {
     @Transactional
     public void updateActuatorState(Long userId, Long groupsId, Long actuatorId, Map<String, Object> newState) {
         groupMemberService.validateGroupAdmin(groupsId, userId);
+
+        // 명령 값 검증을 위해 actuatorType이 필요 - 조회하면서 소유권 검증도 같이 됨
+        ActuatorResponse actuator = actuatorService.getActuatorById(groupsId, actuatorId);
+        ActuatorCommandPreset.validateCommandValues(actuator.actuatorType(), newState);
+
         // 컨트롤러를 통한 사용자 요청이므로 항상 USER + 호출자 userId를 실행이력에 남김
         actuatorService.updateActuatorState(groupsId, actuatorId, newState, ExecutedByType.USER, userId);
     }
