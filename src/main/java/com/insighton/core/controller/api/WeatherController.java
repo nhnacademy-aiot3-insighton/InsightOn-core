d6c4ad01d23ecbb0aad8f0ae146ed684f87e90a8
dayea11 <dayea021128@gmail.com>
2026-08-13 17:57:43 +0900

    feat: 서버가 정상적으로 운영될 때 사용하는 날씨 조회 컨트롤러

---
+++ src/main/java/com/insighton/core/controller/api/WeatherController.java
@@ -0,0 +1,33 @@
+package com.insighton.core.controller.api;
+
+import com.insighton.core.domain.weather.dto.WeatherDataDto;
+import com.insighton.core.domain.weather.service.WeatherCacheService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/weather")
+public class WeatherController {
+
+    private final WeatherCacheService weatherCacheService;
+
+    @GetMapping
+    public ResponseEntity<WeatherDataDto> getWeather(
+            @RequestParam int gridX,
+            @RequestParam int gridY,
+            @RequestParam String sidoName,
+            @RequestParam String cityName,
+            @RequestParam String baseDate,
+            @RequestParam String baseTime
+    ) {
+        WeatherDataDto weatherDataDto = weatherCacheService.getWeatherDate(
+                gridX, gridY, sidoName, cityName, baseDate, baseTime);
+
+        return ResponseEntity.ok(weatherDataDto);
+    }
+}
