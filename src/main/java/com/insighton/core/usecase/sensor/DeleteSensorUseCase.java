bf5802379bbc234514eb324f10607415664c1ed7
JJ <wognlrla1@gmail.com>
2026-08-18 14:08:04 +0900

    revert: 룰엔진 삭제 이벤트 발행 코드 제거
    ActuatorDeletedEvent/SensorDeletedEvent는 refect/sensortoengine 브랜치에만
    있어서 이 브랜치에서는 컴파일이 깨짐. 장소 미배정 센서 조회 기능만 필요해서
    이벤트 발행 부분만 제외.

--- src/main/java/com/insighton/core/usecase/sensor/DeleteSensorUseCase.java
+++ src/main/java/com/insighton/core/usecase/sensor/DeleteSensorUseCase.java
@@ -2,10 +2,8 @@ package com.insighton.core.usecase.sensor;
 
 import com.insighton.core.common.annotation.UseCase;
 import com.insighton.core.domain.groupmember.service.GroupMemberService;
-import com.insighton.core.domain.sensors.event.SensorDeletedEvent;
 import com.insighton.core.domain.sensors.service.SensorService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.transaction.annotation.Transactional;
 
 @UseCase
@@ -13,15 +11,11 @@ import org.springframework.transaction.annotation.Transactional;
 public class DeleteSensorUseCase {
     private final GroupMemberService groupMemberService;
     private final SensorService sensorService;
-    private final ApplicationEventPublisher eventPublisher;
 
     @Transactional
     public void deleteSensor(Long userId, Long sensorId) {
         Long groupId = sensorService.getSensorGroupId(sensorId);
         groupMemberService.validateGroupAdmin(groupId, userId);
         sensorService.deleteSensor(sensorId);
-
-        // 센서 삭제시 룰엔진에게 보낼 이벤트
-        eventPublisher.publishEvent(new SensorDeletedEvent(sensorId));
     }
 }
