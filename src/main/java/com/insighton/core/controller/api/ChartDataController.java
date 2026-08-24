de02282a22a9051a5f03f867bd5b6a906c29c189
{naeun912} <{st243235@gmail.com}>
2026-08-24 16:13:45 +0900

    feat: chartdataController에 user와 group 아이디, location Id의 검증 추가

--- src/main/java/com/insighton/core/controller/api/ChartDataController.java
+++ src/main/java/com/insighton/core/controller/api/ChartDataController.java
@@ -1,8 +1,7 @@
 package com.insighton.core.controller.api;
 
-import com.insighton.core.domain.groupmember.service.GroupMemberService;
 import com.insighton.core.domain.widgets.dto.chart.ChartDataResponse;
-import com.insighton.core.domain.widgets.service.WidgetService;
+import com.insighton.core.usecase.chartdata.ChartDataUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class ChartDataController {
-    private final WidgetService widgetService;
-    private final GroupMemberService groupMemberService;
+    private final ChartDataUseCase chartDataUseCase;
 
     /**
      * chart.js에서 주기적으로 호출할 API
@@ -22,14 +20,15 @@ public class ChartDataController {
      * @param widgetId 관련 Widget ID
      * @return influxDB에서 query문을 날려 가져온 값을 dto에 담아 반환
      */
-    @GetMapping("/api/v1/dashboard/widgets/{widget-id}/chart-data")
-    public ResponseEntity<ChartDataResponse> getWidgetChartData(@RequestHeader("X-User-Id") Long userId,
-                                                                @PathVariable("widget-id") Long widgetId) {
+    @GetMapping("/api/v1/groups/{group-id}/location/{location-id}/dashboard/widgets/{widget-id}/chart-data")
+    public ResponseEntity<ChartDataResponse> getWidgetChartData(
+            @RequestHeader("X-USER-ID") Long userId,
+            @PathVariable("group-id") Long groupId,
+            @PathVariable("location-id") Long locationId,
+            @PathVariable("widget-id") Long widgetId
+    ) {
 
-        Long groupId = widgetService.getWidgetGroupId(widgetId);
-        groupMemberService.validateGroupMembers(groupId, userId);
-
-        ChartDataResponse response = widgetService.getWidgetChartData(widgetId);
-        return ResponseEntity.ok(response);
+        ChartDataResponse chartDataResponse = chartDataUseCase.getWidgetChartData(userId, groupId, locationId, widgetId);
+        return ResponseEntity.ok(chartDataResponse);
     }
 }
